7357eedd8ebd03bed33fb3ab59e625c6642ddffd
Mochi <mochi@egghead-2.local>
2026-03-14 14:18:08 +0800

    Minor fixes
    Co-Authored-By: Jennifer Bautista <72844242+jlusong15@users.noreply.github.com>

--- app/dashboard/RandomTrivia/page.tsx
+++ app/dashboard/RandomTrivia/page.tsx
@@ -11,7 +11,7 @@ export default function RandomTrivia() {
 	return (
 		<div className="p-2 text-center">
 			<h3 className="text-lg font-semibold mb-2">Random Trivia</h3>
-			<p className="text-gray-700">{randomTrivia.fact}</p>
+			<p className="text-gray-700">{randomTrivia.fact ?? ''}</p>
 		</div>
 	)
 }
